da046590d8019e54e34e0a73e8a674df232d75de
dmay <dave.mayhem23@gmail.com>
2024-09-20 22:31:09 +0300

    Finalizing testing

--- ex3.c
+++ ex3.c
@@ -1,19 +1,44 @@
 
 #include <petsc.h>
 
+PetscErrorCode RHSFunction_seq(TS ts, PetscReal t, Vec U, Vec F, void *ctx);
 PetscErrorCode RHSFunction_hip(TS ts, PetscReal t, Vec U, Vec F, void *ctx);
+PetscErrorCode xRHSFunction_hip(TS ts, PetscReal t, Vec U, Vec F, void *ctx);
 
-int main(int argc, char **argv)
+
+PetscErrorCode ex4_1(void)
+{
+  Vec          U;
+  PetscInt     i, len = 5;
+  MPI_Comm     comm;
+
+  PetscFunctionBeginUser;
+
+  comm = PETSC_COMM_WORLD;
+  PetscCall(VecCreate(comm, &U));
+  PetscCall(VecSetSizes(U, len, PETSC_DETERMINE));
+  PetscCall(VecSetType(U,VECSTANDARD));
+  PetscCall(VecSetFromOptions(U));
+  PetscCall(VecSetUp(U));
+
+  PetscCall(VecSet(U,2.0));
+  PetscCall(VecView(U,PETSC_VIEWER_STDOUT_WORLD));
+
+  PetscCall(VecDestroy(&U));
+
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+PetscErrorCode xxx(void)
 {
   TS           ts; /* ODE integrator */
   Vec          U;  /* solution will be stored here */
   PetscMPIInt  commsize;
-  PetscInt     i, len = 5;
+  PetscInt     i, len = 1024*1024;
   PetscScalar  *u = NULL;
   MPI_Comm     comm;
 
   PetscFunctionBeginUser;
-  PetscCall(PetscInitialize(&argc, &argv, (char *)0, NULL));
 
   comm = PETSC_COMM_WORLD;
   PetscCallMPI(MPI_Comm_size(comm, &commsize));
@@ -23,18 +48,20 @@ int main(int argc, char **argv)
   PetscCall(TSSetType(ts, TSRK));
   PetscCall(TSSetProblemType(ts, TS_NONLINEAR));
 
-  PetscCall(TSSetRHSFunction(ts, NULL, RHSFunction_hip, NULL));
+  PetscCall(TSSetRHSFunction(ts, NULL, RHSFunction_seq, NULL));
 
   /* initial condition */
   PetscCall(VecCreate(comm, &U));
   PetscCall(VecSetSizes(U, len, PETSC_DETERMINE)); // slip-rate + state
+  PetscCall(VecSetType(U,VECSTANDARD));
   PetscCall(VecSetFromOptions(U));
   PetscCall(VecSetUp(U));
+
 
   PetscCall(VecGetLocalSize(U,&len));
   PetscCall(VecGetArray(U, &u));
   for (i=0; i<len; i++) {
-    u[i] = 0.0;
+    u[i] = 1.0;
   }
   PetscCall(VecRestoreArray(U, &u));
 
@@ -49,12 +76,31 @@ int main(int argc, char **argv)
 
   PetscCall(TSSolve(ts, U));
 
-  VecView(U,PETSC_VIEWER_STDOUT_(comm));
+  Vec F;
+  VecDuplicate(U,&F);
+  VecSet(F,2.0);
+  VecAXPY(U,1.0,F);
+  //VecView(U,PETSC_VIEWER_STDOUT_(comm));
+
+  //printf("single call\n");
+  //PetscCall(xRHSFunction_hip(ts,0.0, U,F,NULL));
+
 
   PetscCall(VecDestroy(&U));
   PetscCall(TSDestroy(&ts));
 
-  PetscCall(PetscFinalize());
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
 
+int main(int argc, char **argv)
+{
+  PetscFunctionBeginUser;
+  PetscCall(PetscInitialize(&argc, &argv, (char *)0, NULL));
+
+  //PetscCall(ex4_1());
+  PetscCall(xxx());
+
+  PetscCall(PetscFinalize());
   return 0;
 }
+
--- ex3_res.cpp
+++ ex3_res.cpp
@@ -2,24 +2,46 @@
 #include <petsc.h>
 #include <hip/hip_runtime.h>
 
-#define WARPS_PER_BLOCK 4
+#define GRID_MAX 1024
+#define THREAD_MAX 1024
+
+
+void build_sizes(long int N,dim3 *b, dim3 *t)
+{
+  if (N < GRID_MAX * THREAD_MAX) {
+    t->x = THREAD_MAX; t->y = 1; t->z = 1;
+    b->x = (N + GRID_MAX-1)/GRID_MAX; b->y = 1; b->z = 1;
+    return;
+  }
+
+  if (N < GRID_MAX * GRID_MAX * THREAD_MAX) {
+    long int bJ = (N-1)/(GRID_MAX*GRID_MAX);
+    t->x = THREAD_MAX; t->y = 1; t->z = 1;
+    b->x = GRID_MAX; b->y = bJ; b->z = 1;
+
+  }
+
+}
+
+
+//#define HCC_ENABLE_PRINTF
 
 __global__ void __RHSFunction_hip(PetscInt len, PetscScalar f[])
 {
-  //int i, idx_in_warp = hipThreadIdx_x;//idx_in_warp = threadIdx.x % 32;
-  int i, idx_in_warp = idx_in_warp = threadIdx.x % 32;
+  int i = threadIdx.x + blockIdx.x*blockDim.x;
 
+  //printf("block index %d %d %d | thread index %d,%d,%d --> i %d\n",(int)blockIdx.x,(int)blockIdx.y,(int)blockIdx.z,(int)threadIdx.x,(int)threadIdx.y,(int)threadIdx.z, i);
+  if (i >= len) return;
 
-  if (idx_in_warp >= len) return;
-
-  i = idx_in_warp;
+    //printf("  i %d (b %d -> t %d)\n",i,(int)blockIdx.x,(int)threadIdx.x);
 
     f[i] = (PetscScalar)i + 0.1;
 }
 
 extern "C" {
 
-PetscErrorCode RHSFunction_hip(TS ts, PetscReal t, Vec U, Vec F, void *ctx)
+
+PetscErrorCode RHSFunction_seq(TS ts, PetscReal t, Vec U, Vec F, void *ctx)
 {
   PetscScalar           *f;
   const PetscScalar     *u;
@@ -34,9 +56,32 @@ PetscErrorCode RHSFunction_hip(TS ts, PetscReal t, Vec U, Vec F, void *ctx)
   for (i=0; i<len; i++) {
     f[i] = (PetscScalar)i + 0.1;
   }
-
   PetscCall(VecRestoreArrayRead(U, &u));
   PetscCall(VecRestoreArray(F, &f));
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+PetscErrorCode RHSFunction_hip(TS ts, PetscReal t, Vec U, Vec F, void *ctx)
+{
+  PetscScalar           *f;
+  const PetscScalar     *u;
+  PetscInt              i,len;
+
+  PetscFunctionBeginUser;
+  PetscCall(VecGetLocalSize(U,&len));
+
+  //PetscCall(VecGetArrayRead(U, &u));
+  //PetscCall(VecGetArray(F, &f));
+  PetscCall(VecHIPGetArrayRead(U, &u));
+  PetscCall(VecHIPGetArray(F, &f));
+
+  for (i=0; i<len; i++) {
+    f[i] = (PetscScalar)i + 0.1;
+  }
+  PetscCall(VecHIPRestoreArrayRead(U, &u));
+  PetscCall(VecHIPRestoreArray(F, &f));
+  //PetscCall(VecRestoreArrayRead(U, &u));
+  //PetscCall(VecRestoreArray(F, &f));
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
@@ -46,18 +91,33 @@ PetscErrorCode xRHSFunction_hip(TS ts, PetscReal t, Vec U, Vec F, void *ctx)
   int ierr;
   PetscScalar           *f;
   const PetscScalar     *u;
-  PetscInt              len;
-  PetscMemType          mt;
+  PetscInt              i,len;
+  //PetscMemType          mt;
 
+  PetscFunctionBeginUser;
   PetscCall(VecGetLocalSize(U,&len));
 
-  PetscCall(VecGetArrayReadAndMemType(U, &u, &mt));
-  PetscCall(VecGetArrayAndMemType(F, &f, &mt));
+  //PetscCall(VecGetArrayReadAndMemType(U, &u, &mt));
+  //PetscCall(VecGetArrayAndMemType(F, &f, &mt));
+  PetscCall(VecHIPGetArrayRead(U, &u));
+  PetscCall(VecHIPGetArray(F, &f));
 
   //__RHSFunction_hip <<< len/WARPS_PER_BLOCK+1, WARPS_PER_BLOCK*32 >>> (len, f);
 
-  PetscCall(VecRestoreArrayReadAndMemType(U, &u));
-  PetscCall(VecRestoreArrayAndMemType(F, &f));
+  // Maximum Block Dimensions: 1024 x 1024 x 1024
+  //Maximum Threads Per Block: 1024
+  dim3 threads(1024,1,1);
+  dim3 blocks((len+1024-1)/1024,1,1);
+  __RHSFunction_hip <<< blocks, threads, 0, 0 >>> (len, f);
+
+  //for (i=0; i<len; i++) {
+  //  f[i] = (PetscScalar)i + 0.1;
+  //}
+
+  PetscCall(VecHIPRestoreArrayRead(U, &u));
+  PetscCall(VecHIPRestoreArray(F, &f));
+  //PetscCall(VecRestoreArrayReadAndMemType(U, &u));
+  //PetscCall(VecRestoreArrayAndMemType(F, &f));
 
   //ierr = hipDeviceSynchronize();
   PetscFunctionReturn(PETSC_SUCCESS);
